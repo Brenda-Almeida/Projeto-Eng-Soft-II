b2b4260c112e12d9a2fa963b7d2effd56cf8bf32
Brenda Almeida <brendabdg000@gmail.com>
2021-05-24 19:09:54 -0300

    migration table subject

---
+++ src/database/migrations/1621891753103-tableSubject.ts
@@ -0,0 +1,61 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class tableSubject1621891753103 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "tb_subject",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                       name: "name",
+                       type: "varchar" 
+                    }, 
+                    {
+                        name: "id_teacher",
+                        type: "uuid",
+                        isNullable: true,
+                    },
+                    {
+                        name: "period",
+                        type: "varchar",
+                    },
+                    {
+                        name: "status",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FKIdTeacher",
+                        referencedTableName: "tb_users",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["id_teacher"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("tb_subject");
+    }
+
+}
