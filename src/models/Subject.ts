b674d4dc7d04664b63c90c72efa9dddb12f783c1
Brenda Almeida <brendabdg000@gmail.com>
2021-05-25 15:51:26 -0300

    Subject Entity

---
+++ src/models/Subject.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+import User from "./User"
+
+@Entity('tb_subjects')
+class Subject {
+
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @JoinColumn({ name: "id_teacher"})
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  id_teacher: string;
+
+  @Column()
+  period: string;
+
+  @Column()
+  status: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
+export default Subject;
