7c974a302c826060dd2403177451e21d8f38a932
MateusNazare <mateus.nazare5@gmail.com>
2021-05-25 16:18:26 -0300

    model topic criada

---
+++ src/models/Topic.ts
@@ -0,0 +1,49 @@
+import {
+    Entity,
+    Column,
+    PrimaryColumn,
+    ManyToOne,
+    JoinColumn,
+    UpdateDateColumn,
+    CreateDateColumn,
+} from 'typeorm';
+
+import { Subject } from "./Subject";
+
+import {
+    v4 as uuid
+} from "uuid"
+
+@Entity('topic')
+class Topic {
+
+    @PrimaryColumn()
+    id: string;
+
+    @JoinColumn({ name: "subject_id" })
+    @ManyToOne(() => Subject)    
+    subject: Subject;
+
+    @Column()
+    subject_id: string;
+    
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+    
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Topic };
