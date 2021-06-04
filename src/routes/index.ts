d80c60b98eab6b103a88b19bfbf9df1e7fb32d60
VistoCraft <victorprezia@hotmail.com.br>
2021-06-04 16:29:26 -0300

    Rota do user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { SubjectsController } from "../controllers/SubjectsController";
 import { TopicController } from '../controllers/TopicController';
-
-import usersRouter from './users.routes';
+import { UserController } from '../controllers/UserController';
 
 const routes = Router();
 
+const userController = new UserController();
+
+routes.post("/user", userController.create);
+
 const subjectsController = new SubjectsController();
 
-routes.use('/users', usersRouter);
 routes.post("/subjects", subjectsController.create);
 
 // =============== topic routes =================
